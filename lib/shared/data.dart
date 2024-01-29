441117575b30bf28d57d8d64473ea78f7dc034c0
Vic Rubba <crazedVic@users.noreply.github.com>
2024-01-29 15:44:04 -0700

    comment

--- lib/shared/data.dart
+++ lib/shared/data.dart
@@ -1,3 +1,4 @@
+// grab your api key from https://api-ninjas.com/profile
 const apiKey= '7prPdiUVjxesPaxW/DUnNQ==wNaG3BdePcwZTGqI';
 const baseUrl = 'api.api-ninjas.com';
 const DataSource dataAccessMode = DataSource.localdb;
